ae0446785709c75b2ecaa21f4c5d3d05fd2f65fd
cxmarcelo <marcelo.c.b22@gmail.com>
2022-10-08 16:06:22 -0300

    Jsx exemplo base.

---
+++ pages/basics/second.js
@@ -0,0 +1,9 @@
+//JSX
+//O NEXTJS nao precisa importar o React from "react" para funcionar as tags html.
+export default function second() {
+    return (
+        <div>
+            <h1>Second</h1>
+        </div>
+    );
+}
